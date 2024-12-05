fc240fab8da25f3d4863ab3c52545de84e9845d9
Alex Stephens <astephens7000@yahoo.com>
2024-12-04 21:51:41 -0500

    day 3 part 2

--- Day3/mull-it-over.js
+++ Day3/mull-it-over.js
@@ -2,11 +2,41 @@ function mul(lhs, rhs) {
     return lhs * rhs;
 }
 
-function parseForMul(str) {
+function getHighestIndexUpTo(indexes, maxIndex) {
+    let ret = -1;
+    for (let i = 0; i < indexes.length; i++) {
+        let ind = indexes[i];
+        if (ind < maxIndex) {
+            ret = ind;
+        } else {
+            break;
+        }
+    }
+    return ret;
+}
+
+function parseForMul(str, initialDo) {
     let matches = str.matchAll(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/g);
+    let dontInstrs = str.matchAll(/don\'t\(\)/g);
+    let donts = [];
+    for (const instr of dontInstrs) {
+        donts.push(instr.index);
+    }
+    let doInstrs = str.matchAll(/do\(\)/g);
+    let dos = [];
+    for (const instr of doInstrs) {
+        dos.push(instr.index);
+    }
     let ret = [];
     for (const match of matches) {
-        ret.push([Number(match[1]), Number(match[2])]);
+        let dontIndex = getHighestIndexUpTo(donts, match.index);
+        let doIndex = getHighestIndexUpTo(dos, match.index);
+        if (doIndex >= 0 || dontIndex >= 0) {
+            initialDo.value = doIndex > dontIndex;
+        }
+        if (initialDo.value) {
+            ret.push([Number(match[1]), Number(match[2])]);
+        }
     }
     return ret;
 }
@@ -19,8 +49,9 @@ function readInput(filepath) {
 
 let output = readInput("./input.txt");
 let total = 0;
+let initialDoInstr = { value: true };
 for (const out of output) {
-    let args = parseForMul(out);
+    let args = parseForMul(out, initialDoInstr);
     for (const arg of args) {
         total += mul(arg[0], arg[1]);
     }
