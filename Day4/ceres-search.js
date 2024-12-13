4231b7d7e718003bce888be84d5f7dc38c2f7954
Alex Stephens <astephens7000@yahoo.com>
2024-12-13 10:46:55 -0500

    day 4 part 2

--- Day4/ceres-search.js
+++ Day4/ceres-search.js
@@ -1,6 +1,6 @@
 let numRows, numCols;
-let dx = [-1, -1, -1, 0, 0, 1, 1, 1];
-let dy = [-1, 0, 1, -1, 1, -1, 0, 1];
+let dx = [-1, -1, -1, 0,  0,  1, 1, 1];
+let dy = [-1,  0,  1, -1, 1, -1, 0, 1];
 
 function searchWord(grid, r, c, word) {
     let ret = 0;
@@ -36,6 +36,49 @@ function searchWord(grid, r, c, word) {
     return ret;
 }
 
+function searchXMAS(grid, r, c) {
+    if (grid[r][c] !== "A") {
+        return 0;
+    }
+
+    if (grid[r + dx[0]][c + dy[0]] === grid[r + dx[7]][c + dy[7]]) {
+        return 0;
+    }
+    if (grid[r + dx[2]][c + dy[2]] === grid[r + dx[5]][c + dy[5]]) {
+        return 0;
+    }
+
+    let isCross = true;
+    if (grid[r + dx[0]][c + dy[0]] !== grid[r + dx[7]][c + dy[7]]) {
+        let isValid = (grid[r + dx[0]][c + dy[0]] == "M" || grid[r + dx[0]][c + dy[0]] == "S");
+        isValid = isValid && (grid[r + dx[7]][c + dy[7]] == "M" || grid[r + dx[7]][c + dy[7]] == "S")
+        if (!isValid) {
+            return 0;
+        }
+        isCross = isCross && isValid;
+    }
+    if (grid[r + dx[2]][c + dy[2]] !== grid[r + dx[5]][c + dy[5]]) {
+        let isValid = (grid[r + dx[2]][c + dy[2]] == "M" || grid[r + dx[2]][c + dy[2]] == "S");
+        isValid = isValid && (grid[r + dx[5]][c + dy[5]] == "M" || grid[r + dx[5]][c + dy[5]] == "S")
+        if (!isValid) {
+            return 0;
+        }
+        isCross = isCross && isValid;
+    }
+
+    return isCross;
+}
+
+function findCross(grid) {
+    let numMatches = 0;
+    for (let r = 1; r < numRows - 1; r++) {
+        for (let c = 1; c < numCols - 1; c++) {
+            numMatches += searchXMAS(grid, r, c);
+        }
+    }
+    return numMatches;
+}
+
 function findPattern(grid, targetWord) {
     let numMatches = 0;
     for (let r = 0; r < numRows; r++) {
@@ -61,3 +104,4 @@ numRows = output.length;
 numCols = output[0].length;
 console.log("Size: " + numRows + "x" + numCols);
 console.log(findPattern(output, "XMAS"));
+console.log(findCross(output));
